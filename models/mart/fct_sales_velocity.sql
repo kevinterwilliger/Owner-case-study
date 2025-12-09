c1064be7821a01722aadb580f1be7bd4b414f004
Kevin Terwilliger <kevin.terwilliger17@gmail.com>
2025-12-08 19:52:55 -0800

    Update fct_sales_velocity.sql

--- models/mart/fct_sales_velocity.sql
+++ models/mart/fct_sales_velocity.sql
@@ -13,7 +13,11 @@ SELECT
 
     , form_submission_date
     , initial_engagement_date
-    , DATEDIFF(DAY, form_submission_date, initial_engagement_date) AS inbound_to_lead_days
+    , DATEDIFF(
+        DAY
+        , initial_inbound_date
+        , form_submission_date
+    ) AS inbound_to_lead_days
 
     , (sales_call_count + sales_email_count + sales_text_count) AS total_touchpoints
     , sales_call_count
@@ -23,11 +27,11 @@ SELECT
     , opp_created_date
     , close_date
 
-    , DATEDIFF(day, form_submission_date, opp_created_date) AS lead_to_opp_days
+    , DATEDIFF(day, initial_engagement_date, opp_created_date) AS lead_to_opp_days
     
     , DATEDIFF(day, opp_created_date, close_date) AS opp_to_close_days
     
-    , DATEDIFF(day, form_submission_date, close_date) AS total_sales_cycle_days
+    , DATEDIFF(day, initial_engagement_date, close_date) AS total_sales_cycle_days
 
     , predicted_monthly_sales
     , subscription_ltv
